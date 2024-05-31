166b62e3ec64e159c56b1dbc902e3d1a5ffb2b3e
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-05-31 21:41:36 +0300

    feat/styles: @ as root path, more button styles

--- www/components/ui/Button.tsx
+++ www/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
 
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
@@ -21,8 +21,10 @@ export default function Button({
 const buttonVariants = cva("py-2 px-4 rounded font-medium", {
   variants: {
     variant: {
-      primary: "bg-black text-white",
-      secondary: "border border-black border-opacity-30",
+      primary:
+        "bg-black text-white duration-200 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-300",
+      secondary:
+        "border border-black border-opacity-30 duration-200 text-black hover:text-gray-800 hover:border-opacity-50 dark:text-white dark:hover:text-gray-300 dark:border-white dark:border-opacity-40 dark:hover:border-opacity-50",
     },
   },
   defaultVariants: {
